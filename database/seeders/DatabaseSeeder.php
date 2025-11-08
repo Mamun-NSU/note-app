d118789ab15cf57338d251b35d405f873b3256aa
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-11-08 10:41:15 +0600

    Model, controller, route and environment of Laravel note-app created successfully

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+
+
+        User::firstOrCreate([
+                'id' => 1,
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => bcrypt('pass123.')
+            ]);
+
+            Note::factory(100) ->create();
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\NoteController;
+
+
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+
+
+// Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+// Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+// Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+// Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+// Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+
+Route::resource('note', NoteController::class);
+
+
+
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
+
+// require __DIR__.'/settings.php';

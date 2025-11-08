54a45168670be4f756aa59067a27fc33be3a4200
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-11-08 13:25:57 +0600

    Login & Registration with data validations, csrf token with sessions management of Laravel note-app created successfully

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,36 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
-use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
-
-
-
-// Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
-// Route::post('/note', [NoteController::class, 'store'])->name('note.store');
-// Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
-// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
-// Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
-// Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
-
-
-Route::resource('note', NoteController::class);
-
-
-
-
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
+// Route::get('/', function () {
+//     return view('welcome');
 // });
 
-// require __DIR__.'/settings.php';
+Route::redirect('/', '/note')->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+    // Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+    // Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+    // Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+    // Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+    // Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+    // Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+    Route::resource('note', NoteController::class);
+});
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
